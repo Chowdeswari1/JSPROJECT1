0aefd1201e5ea9857461ae581ce268bbde81e38c
Chowdeswari1 <68473303+Chowdeswari1@users.noreply.github.com>
2021-01-25 12:17:02 +0530

    JS PROJECT FINAL

---
+++ jsscript1.js
@@ -0,0 +1,136 @@
+function Deletetask1()
+{
+    let text = document.getElementsByClassName("projects");
+    let change=text[0].style.visibility="hidden";
+   }
+   function Deletetask2()
+{
+    let text = document.getElementsByClassName("projects");
+    let change=text[1].style.visibility="hidden";
+   }
+   function Deletetask3()
+{
+    let text = document.getElementsByClassName("projects");
+    let change=text[2].style.visibility="hidden";
+   }
+   function Deletetask4()
+{
+    let text = document.getElementsByClassName('projects');
+    let change=text[3].style.visibility="hidden";
+   }
+   function Deletetask5()
+{
+    let text = document.getElementsByClassName('projects');
+    let change=text[4].style.visibility="hidden";
+   }
+   function Deletetask6()
+{
+    let text = document.getElementsByClassName('projects');
+    let change=text[5].style.visibility="hidden";
+   }
+   function Deletetask7()
+{
+    let text = document.getElementsByClassName('projects');
+    let change=text[6].style.visibility="hidden";
+   }
+
+   function complete1()
+   {
+       let text=document.getElementsByClassName('projects');
+       let changetext=text[0].innerHTML="Task completed";
+       let change=text[0].style.color="darkgreen";
+   }
+   
+   function complete2()
+   {
+       let text=document.getElementsByClassName('projects');
+       let changetext=text[1].innerHTML="Task completed";
+       let change=text[1].style.color="darkgreen";
+   }
+
+   function complete3()
+   {
+       let text=document.getElementsByClassName('projects');
+       let changetext=text[2].innerHTML="Task completed";
+       let change=text[2].style.color="darkgreen";
+   }
+   
+   function complete4()
+   {
+       let text=document.getElementsByClassName('projects');
+       let changetext=text[3].innerHTML="Task completed";
+       let change=text[3].style.color="darkgreen";
+   }
+   
+   function complete5()
+   {
+       let text=document.getElementsByClassName('projects');
+       let changetext=text[4].innerHTML="Task completed";
+       let change=text[4].style.color="darkgreen";
+   }
+   
+   function complete6()
+   {
+       let text=document.getElementsByClassName('projects');
+       let changetext=text[5].innerHTML="Task completed";
+       let change=text[5].style.color="darkgreen";
+   }
+   
+   function complete7()
+   {
+       let text=document.getElementsByClassName('projects');
+       let changetext=text[6].innerHTML="Task completed";
+       let change=text[6].style.color="darkgreen";
+   }
+   
+   function not1()
+   {
+    let text=document.getElementsByClassName('projects');
+    let changetext=text[0].innerHTML="Do it fast";
+    let change=text[0].style.color="Red";
+}
+function not2()
+{
+ let text=document.getElementsByClassName('projects');
+ let changetext=text[1].innerHTML="Do it fast";
+ let change=text[1].style.color="Red";
+function not3()
+{
+ let text=document.getElementsByClassName('projects');
+ let changetext=text[2].innerHTML="Do it fast";
+ let change=text[2].style.color="Red";
+}
+function not4()
+{
+ let text=document.getElementsByClassName('projects');
+ let changetext=text[3].innerHTML="Do it fast";
+ let change=text[3].style.color="Red";
+}
+function not5()
+{
+ let text=document.getElementsByClassName('projects');
+ let changetext=text[4].innerHTML="Do it fast";
+ let change=text[4].style.color="Red";
+}
+function not6()
+{
+ let text=document.getElementsByClassName('projects');
+ let changetext=text[5].innerHTML="Do it fast";
+ let change=text[5].style.color="Red";
+}
+function not7()
+{
+ let text=document.getElementsByClassName('projects');
+ let changetext=text[6].innerHTML="Do it fast";
+ let change=text[6].style.color="Red";
+}
+function final()
+{
+    let hide=document.getElementById("hide");
+    let xyz=hide.style.visibility="hidden";
+    let fin=document.getElementById("final");
+   let yz=fin.style.visibility="hidden";
+   var bas=document.getElementById("head");
+   var changebas=bas.innerHTML="YOUR CHECK LIST HAS BEEN UPDATED!!";
+}
+}
